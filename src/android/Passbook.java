c4c03464f25fbecade85e6ab7690c631c0c11df4
luisvoliveira <52253482+luisvoliveira@users.noreply.github.com>
2023-07-21 22:52:00 +0100

    Add files via upload

---
+++ src/android/Passbook.java
@@ -0,0 +1,25 @@
+package com.example.plugin;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class Passbook extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+		
+    }
+	
+    public boolean available(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+		
+    }
+	
+    public boolean downloadPass(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+		
+    }
+	
+    public boolean addPass(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+		
+    }
+}
